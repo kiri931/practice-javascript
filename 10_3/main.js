409143a5ad57e8b39f024880223bc8b13d2c8abc
kiri931 <44487096+kiri931@users.noreply.github.com>
2024-10-04 05:21:31 +0000

    10月4日5限目

---
+++ 10_3/main.js
@@ -0,0 +1,21 @@
+//クリックされたら動作する
+
+function errorInput(){
+    console.log(document.getElementById("month").value)
+    console.log(document.getElementById("day").value)
+
+    const month = document.getElementById("month").value;
+    const day = document.getElementById("day").value
+    
+    console.log("month:",month,"day:",day)
+
+}
+//document.getElementById("month")はタグを確認するだけ
+//valueはそのタグの値を示す
+
+/*
+javaScriptのプログラムとHTMLファイルはどこで対応しているか
+タグについているIDで紐づく
+タグの中にある付加情報のことを属性という
+console.log()はc言語でいうところのprintf();です
+*/
--- main.js
+++ main.js
@@ -1,21 +1,9 @@
-//クリックされたら動作する
-
 function errorInput(){
-    console.log(document.getElementById("month").value)
-    console.log(document.getElementById("day").value)
-
     const month = document.getElementById("month").value;
-    const day = document.getElementById("day").value
-    
-    console.log("month:",month,"day:",day)
-
+    const day = document.getElementById("day").value;
+    if(month<1 || month>12){
+        alert("適切な値ではありません");
+        return
+    }
+    console.log("Month:", month, "Day:", day);
 }
-//document.getElementById("month")はタグを確認するだけ
-//valueはそのタグの値を示す
-
-/*
-javaScriptのプログラムとHTMLファイルはどこで対応しているか
-タグについているIDで紐づく
-タグの中にある付加情報のことを属性という
-console.log()はc言語でいうところのprintf();です
-*/
