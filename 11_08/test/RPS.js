f2a4ddede62351eb5b566ff8611e82d14720decf
kiri931 <44487096+kiri931@users.noreply.github.com>
2024-11-08 03:05:11 +0000

    じゃんけん研究授業

---
+++ 11_08/test/RPS.js
@@ -0,0 +1,29 @@
+
+function RockPaperScissors() {
+  const choices = ["gu","choki","pa"];
+
+  let select = document.createElement('select');
+  choices.forEach(choice => {
+    let option = document.createElement('option');
+    option.value = choice.toLowerCase();
+    option.textContent = choice;
+    select.appendChild(option);
+  });
+
+  let button = document.createElement('button');
+    button.textContent = '手を出す';
+    let result = document.createElement('p');
+    let image = document.createElement('img');
+    image.alt = 'Choice';
+    image.style.width = '100px';
+    image.style.height = '100px';
+
+  button.addEventListener('click', function() {
+    const imageName = select.value;
+    image.src = `./image/janken_${imageName}.png`;
+  });
+
+  gameContainer.appendChild(select);
+  gameContainer.appendChild(button);
+  gameContainer.appendChild(image);
+}
--- 11_08/test/clickCount.js
+++ 11_08/test/clickCount.js
@@ -1,24 +1,3 @@
-const gameSelector = document.getElementById('game-selector');
-const gameContainer = document.getElementById('game-container');
-
-gameContainer.textContent="ゲームを選ぶとここに表示されます"
-
-gameSelector.addEventListener("change",function(){
-    gameContainer.innerHTML = "";
-    switch(gameSelector.value){
-        case "none":
-            gameContainer.textContent="ゲームを選ぶとここに表示されます"
-            break;
-        case "click-counter":
-            ClickCounterGame();
-            break;
-        case "number-guess":
-            NumberGuessGame();
-            break;
-    }
-})
-
-
 
 
 function ClickCounterGame(){
