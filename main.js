34fd287beb9acb682760ea2bab56589b4c6e0881
kiri931 <44487096+kiri931@users.noreply.github.com>
2025-01-16 05:17:06 +0000

    ボタンの追加

--- main.js
+++ main.js
@@ -8,7 +8,14 @@ function ClickCounterGame(){
 
     let button1 = document.createElement("button");
     // ボタンのタグを作成する <button>ボタン</button>を作り、変数と紐づける 1
+    let button2 = document.createElement("button");
+    let button3 = document.createElement("button");
+
+
+
     button1.textContent = "+1ボタン"
+    button2.textContent = "10 +"
+    button3.textContent = "リセット"
     // <button>中身の文字をtextContentという</button>
 
     let counter = document.createElement("p");
@@ -22,7 +29,19 @@ function ClickCounterGame(){
     
     })
 
+    button2.addEventListener("click",function(){
+        count = count + 10;
+        counter.textContent = count;
+    })
+
+    counter.addEventListener("click",function(){
+        count = 0;
+        counter.textContent = count;
+    })
+
     gameContainer.appendChild(button1); // ボタンを表示する  4
+    gameContainer.appendChild(button2); 
+    gameContainer.appendChild(button3); 
     gameContainer.appendChild(counter);
 }
 ClickCounterGame();
